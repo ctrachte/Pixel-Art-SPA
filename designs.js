397369df5fd484aaec3fb226b5af0fc926ab775c
ctrachte <cetrachte@hotmail.com>
2018-03-20 00:02:44 -0500

    add starting functions for color picker, grid selector, grid creator and grid delete.

--- designs.js
+++ designs.js
@@ -1,10 +1,36 @@
 // Select color input
+// function selectColor () {
+//   var color = this.color;
+//   return color;
+// };
+
 // Select size input
+
+function selectGridSize () {
+  var x = $('inputWidth').val();
+  var y = $('inputHeight').val();
+  return x, y;
+};
 
 // When size is submitted by the user, call makeGrid()
 
-function makeGrid() {
+function makeGrid(x,y) {
+  for (var rows = 0; rows < x; rows++) {
+    $("#pixelCanvas").append("<tr class='row'></tr>");
+    };
+  for (var columns = 0; columns < y; columns++) {
+    $(".row").append("<td class='col'>x</td>");
+    };
+};
 
-// Your code goes here!
+// Remove the grid if the user is not satisfied:
 
-}
+function removeGrid () {
+  $("#pixelCanvas").children.remove();
+
+};
+
+
+var x, y = selectGridSize();
+$('form').submit(makeGrid(x,y));
+// $('#colorPicker').on('select', selectColor());
